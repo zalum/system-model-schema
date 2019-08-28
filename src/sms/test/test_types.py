792042cac79364b55892602639843fceb75d5100
Florin Zalum <florin.zalum@zooplus.com>
2019-08-28 12:01:24 +0200

    (!) travis tests

--- src/sms/test/test_types.py
+++
@@ -1,7 +0,0 @@
-class A:
-    def __init__(self, name):
-        self.name = name
-
-
-__this_module = globals()
-__this_module["B"] = type("B", (A,), dict(bname=1))
--- src/sms/test/test_types.pyi
+++
@@ -1,9 +0,0 @@
-class A:
-    def __init__(self, name):
-        pass
-
-    name = ...  # type: str
-
-
-class B(A):
-    bname = ...  # type: int
